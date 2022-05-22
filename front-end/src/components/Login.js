6c2c3e95c0cd26c24db9e23b904c73cfef65d3c0
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-22 16:27:32 +0300

    Make register form controled

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 export default function Login() {
+  const [formData, updateFormData] = useState({
+    userName: '',
+    firstName: '',
+    lastName: '',
+  });
   const onchange = (event) => {
-    event.preventDefault();
-    console.log(event.target);
+    const name = event.target.name;
+    const value = event.target.value;
+    updateFormData(oldState => {
+      oldState[name] = value;
+      return ({ ...oldState });
+    });
   }
+
   return (
     <Container>
       <Row>
         <Col sm={6} md={4}>
-          <form action="http://localhost:3001/user/add-new-user" method="post">
+          <form
+            action="http://localhost:3001/user/add-new-user"
+            method="post">
             <div> <label htmlFor="userName">you name here</label>
               <input type="text" name="userName" id="userName"
-                value="Tadesse-Alemayehu"
+                value={formData.userName}
                 onChange={onchange} /></div>
             <div> <label htmlFor="firstName">you name here</label>
-              <input type="text" name="firstName" id="firstName" value="Tadesse"
+              <input type="text" name="firstName" id="firstName"
+                value={formData.firstName}
                 onChange={onchange} /></div>
             <div> <label htmlFor="lastName">you name here</label>
-              <input type="text" name="lastName" id="lastName" value="Alemayehu"
+              <input type="text" name="lastName" id="lastName"
+                value={formData.lastName}
                 onChange={onchange} /></div>
             <button type="submit">submit</button>
           </form>
